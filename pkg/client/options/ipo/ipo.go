81cbfff053594dfd9e34c7713d4ca38cd21745f9
aptd3v <aptd3v@proton.me>
2025-06-22 01:04:14 -0400

    feat: add image pull options to Docker client wrapper

---
+++ pkg/client/options/ipo/ipo.go
@@ -0,0 +1,54 @@
+// package ipo provides options for the image pull.
+package ipo
+
+import (
+	"context"
+	"runtime"
+
+	"github.com/aptd3v/go-contain/pkg/client/auth"
+	"github.com/docker/docker/api/types/image"
+)
+
+// SetImagePullOption is a function that sets a parameter for the image pull.
+type SetImagePullOption func(*image.PullOptions) error
+
+// WithRegistryAuth sets the registry auth for the image pull.
+func WithRegistryAuth(creds auth.Auth) SetImagePullOption {
+	return func(o *image.PullOptions) error {
+		o.RegistryAuth = auth.AuthToBase64(creds)
+		return nil
+	}
+}
+
+// WithPullAllPlatforms sets the pull all platforms associated with the image
+// (e.g., linux/amd64, linux/arm64, etc.).
+func WithPullAllPlatforms() SetImagePullOption {
+	return func(o *image.PullOptions) error {
+		o.All = true
+		return nil
+	}
+}
+
+// WithCurrentPlatform sets the platform for the image pull to the current platform. (GOARCH)
+func WithCurrentPlatform() SetImagePullOption {
+	return func(o *image.PullOptions) error {
+		o.Platform = runtime.GOARCH
+		return nil
+	}
+}
+
+// WithPrivledgeFn sets the privilege function for the image pull.
+func WithPrivledgeFunc(authFn func(ctx context.Context) (string, error)) SetImagePullOption {
+	return func(o *image.PullOptions) error {
+		o.PrivilegeFunc = authFn
+		return nil
+	}
+}
+
+// WithPlatform sets the platform for the image pull.
+func WithPlatform(platform string) SetImagePullOption {
+	return func(o *image.PullOptions) error {
+		o.Platform = platform
+		return nil
+	}
+}
