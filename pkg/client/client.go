a8023adfaa8172a7ec64b8b9ffbb576c0a734e19
aptd3v <aptd3v@proton.me>
2025-06-22 00:59:14 -0400

    feat: add Docker client wrapper implementation

---
+++ pkg/client/client.go
@@ -0,0 +1,18 @@
+package client
+
+import (
+	"github.com/docker/docker/client"
+)
+
+// Client is a wrapper around the docker client.
+type Client struct {
+	wrapped *client.Client
+}
+
+func NewClient() (*Client, error) {
+	wrapped, err := client.NewClientWithOpts(client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, err
+	}
+	return &Client{wrapped: wrapped}, nil
+}
