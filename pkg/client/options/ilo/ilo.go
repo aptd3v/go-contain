5f8250be2dcaa7172047eabca496e7d2567b7986
aptd3v <aptd3v@proton.me>
2025-06-22 01:03:52 -0400

    feat: add image list options to Docker client wrapper

---
+++ pkg/client/options/ilo/ilo.go
@@ -0,0 +1,51 @@
+// package ilo provides options for the image list.
+package ilo
+
+import (
+	"github.com/docker/docker/api/types/image"
+)
+
+// SetImageListOption is a function that sets a parameter for the image list.
+type SetImageListOption func(*image.ListOptions) error
+
+// WithAll sets the all flag for the image list.
+// All controls whether all images in the graph are filtered, or just the heads.
+func WithAll() SetImageListOption {
+	return func(o *image.ListOptions) error {
+		o.All = true
+		return nil
+	}
+}
+
+// WithFilters sets the filters for the image list.
+func WithFilters(key, value string) SetImageListOption {
+	return func(o *image.ListOptions) error {
+		o.Filters.Add(key, value)
+		return nil
+	}
+}
+
+// WithSharedSize sets the shared size for the image list.
+func WithSharedSize() SetImageListOption {
+	return func(o *image.ListOptions) error {
+		o.SharedSize = true
+		return nil
+	}
+}
+
+// WithContainerCount sets the container count for the image list.
+func WithContainerCount() SetImageListOption {
+	return func(o *image.ListOptions) error {
+		o.ContainerCount = true
+		return nil
+	}
+}
+
+// WithManifests sets the manifests for the image list.
+// Manifests indicates whether the image manifests should be returned
+func WithManifests() SetImageListOption {
+	return func(o *image.ListOptions) error {
+		o.Manifests = true
+		return nil
+	}
+}
