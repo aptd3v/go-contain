dcb2d5f9cb846d1f468596a7da471a12ca598ef3
aptd3v <aptd3v@proton.me>
2025-06-22 03:14:53 -0400

    feat: remove q setters to allow any type

--- pkg/tools/tools.go
+++ pkg/tools/tools.go
@@ -1,30 +1,16 @@
 // Package tools provides various helpers for writing declarative option setters.
 package tools
 
-import (
-	"github.com/aptd3v/go-contain/pkg/create"
-)
-
 // PredicateClosure is a condition based on external or ambient context,
 // not the internal config. This allows behavior to be toggled based on
 // CLI flags, environment variables, testing hooks, etc.
 type PredicateClosure func() bool
 
-// QualifiedSetter is a type constraint that ensures the type implements one of the setter interfaces
-type QualifiedSetter interface {
-	create.SetHostConfig | create.SetContainerConfig | create.SetNetworkConfig | create.SetPlatformConfig
-}
-
-// QualifiedConfig is a type constraint that ensures the type implements one of the config interfaces
-type QualifiedConfig interface {
-	*create.ContainerConfig | *create.HostConfig | *create.NetworkConfig | *create.PlatformConfig
-}
-
 // WhenTrue is a function that takes a variadic number of setters and returns a single setter.
 // Only if it passes the provided predicate closures, the setters will be called.
 //
 // note: if any of the setters are nil, they will be skipped and not added to warnings
-func WhenTrue[T QualifiedConfig, O ~func(T) error](predicate PredicateClosure, fns ...O) O {
+func WhenTrue[T any, O ~func(T) error](predicate PredicateClosure, fns ...O) O {
 	return func(t T) error {
 		if !predicate() {
 			return nil
@@ -37,6 +23,14 @@ func WhenTrue[T QualifiedConfig, O ~func(T) error](predicate PredicateClosure, f
 			}
 		}
 		return nil
+	}
+}
+func WhenTrueElse[T any, O ~func(T) error](predicate PredicateClosure, fns O, elseFn O) O {
+	return func(t T) error {
+		if predicate() {
+			return fns(t)
+		}
+		return elseFn(t)
 	}
 }
 
