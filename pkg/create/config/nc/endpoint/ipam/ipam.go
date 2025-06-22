49fdca1c3a898f011f441e043907a9b6bfb80b09
aptd3v <aptd3v@proton.me>
2025-06-22 01:09:11 -0400

    feat: implement IPAM configuration options for endpoint management

---
+++ pkg/create/config/nc/endpoint/ipam/ipam.go
@@ -0,0 +1,44 @@
+// Package ipam provides the options for the IPAM config in the endpoint config.
+package ipam
+
+import (
+	"github.com/docker/docker/api/types/network"
+)
+
+// SetIPAMOptionFn is a function that sets an IPAM option for the network
+type SetIPAMConfig func(opt *network.EndpointIPAMConfig) error
+
+// WithIPv4Address sets the IPv4 address for the IPAM config.
+// If the address is already set, it will be overwritten.
+// Parameter:
+//   - address: the IPv4 address to be used for the network
+func WithIPv4Address(address string) SetIPAMConfig {
+	return func(opt *network.EndpointIPAMConfig) error {
+		opt.IPv4Address = address
+		return nil
+	}
+}
+
+// WithIPv6Address sets the IPv6 address for the IPAM config.
+// If the address is already set, it will be overwritten.
+// Parameter:
+//   - address: the IPv6 address to be used for the network
+func WithIPv6Address(address string) SetIPAMConfig {
+	return func(opt *network.EndpointIPAMConfig) error {
+		opt.IPv6Address = address
+		return nil
+	}
+}
+
+// WithLinkLocalIPs appends the link local IPs for the IPAM config
+// Parameter:
+//   - ips: the link local IPs to be used for the network
+func WithLinkLocalIPs(ips ...string) SetIPAMConfig {
+	return func(opt *network.EndpointIPAMConfig) error {
+		if opt.LinkLocalIPs == nil {
+			opt.LinkLocalIPs = make([]string, 0)
+		}
+		opt.LinkLocalIPs = append(opt.LinkLocalIPs, ips...)
+		return nil
+	}
+}
