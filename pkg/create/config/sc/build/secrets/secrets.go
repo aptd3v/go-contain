be38c5e017583a898d41497000cdb2e9a8d5331d
aptd3v <aptd3v@proton.me>
2025-06-24 03:49:01 -0400

    feat: introduce build configuration functions for Dockerfile, secrets, and ulimits in the sc package

---
+++ pkg/create/config/sc/build/secrets/secrets.go
@@ -0,0 +1,56 @@
+// Package secrets provides a set of functions to configure the secrets for the build
+package secrets
+
+import "github.com/compose-spec/compose-go/types"
+
+type SetSecretConfig func(*types.ServiceSecretConfig) error
+
+// WithSource sets the source for the secret
+// parameters:
+//   - source: the source for the secret
+func WithSource(source string) SetSecretConfig {
+	return func(opt *types.ServiceSecretConfig) error {
+		opt.Source = source
+		return nil
+	}
+}
+
+// WithTarget sets the target for the secret
+// parameters:
+//   - target: the target for the secret
+func WithTarget(target string) SetSecretConfig {
+	return func(opt *types.ServiceSecretConfig) error {
+		opt.Target = target
+		return nil
+	}
+}
+
+// WithUID sets the UID for the secret
+// parameters:
+//   - uid: the UID for the secret
+func WithUID(uid string) SetSecretConfig {
+	return func(opt *types.ServiceSecretConfig) error {
+		opt.UID = uid
+		return nil
+	}
+}
+
+// WithGID sets the GID for the secret
+// parameters:
+//   - gid: the GID for the secret
+func WithGID(gid string) SetSecretConfig {
+	return func(opt *types.ServiceSecretConfig) error {
+		opt.GID = gid
+		return nil
+	}
+}
+
+// WithMode sets the mode for the secret
+// parameters:
+//   - mode: the mode for the secret
+func WithMode(mode uint32) SetSecretConfig {
+	return func(opt *types.ServiceSecretConfig) error {
+		opt.Mode = &mode
+		return nil
+	}
+}
---
+++ pkg/create/config/sc/build/ulimit/ulimit.go
@@ -0,0 +1,36 @@
+// Package ulimit provides a set of functions to configure the ulimits for the build
+package ulimit
+
+import "github.com/compose-spec/compose-go/types"
+
+type SetUlimitConfig func(*types.UlimitsConfig) error
+
+// WithSingle sets the single for the ulimit
+// parameters:
+//   - value: the value for the ulimit
+func WithSingle(value int) SetUlimitConfig {
+	return func(opt *types.UlimitsConfig) error {
+		opt.Single = value
+		return nil
+	}
+}
+
+// WithSoft sets the soft for the ulimit
+// parameters:
+//   - value: the value for the ulimit
+func WithSoft(value int) SetUlimitConfig {
+	return func(opt *types.UlimitsConfig) error {
+		opt.Soft = value
+		return nil
+	}
+}
+
+// WithHard sets the hard for the ulimit
+// parameters:
+//   - value: the value for the ulimit
+func WithHard(value int) SetUlimitConfig {
+	return func(opt *types.UlimitsConfig) error {
+		opt.Hard = value
+		return nil
+	}
+}
