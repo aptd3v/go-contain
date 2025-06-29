822f6ffb14cd4213e4a338616e0e22d13b1170c1
aptd3v <aptd3v@proton.me>
2025-06-29 00:42:53 -0400

    feat: implement context-aware Up, Down, and Logs methods in compose package for improved command execution and error handling

--- pkg/compose/compose.go
+++ pkg/compose/compose.go
@@ -5,6 +5,9 @@
 package compose
 
 import (
+	"context"
+	"errors"
+	"io"
 	"os"
 	"os/exec"
 	"strings"
@@ -22,12 +25,20 @@ func NewCompose(project *create.Project) *compose {
 	}
 }
 
+// SetComposeUpOption is a function that sets a ComposeUpOptions
 type SetComposeUpOption func(*ComposeUpOptions) error
 
-func (c *compose) Up(setters ...SetComposeUpOption) error {
+// SetComposeDownOption is a function that sets a ComposeDownOptions
+type SetComposeDownOption func(*ComposeDownOptions) error
+
+// SetComposeLogsOption is a function that sets a ComposeLogsOptions
+type SetComposeLogsOption func(*ComposeLogsOptions) error
+
+func (c *compose) Up(ctx context.Context, setters ...SetComposeUpOption) error {
 	opt := &ComposeUpOptions{
 		//default flags
-		Flags: []string{"up"},
+		Flags:  []string{"up"},
+		Writer: os.Stdout,
 	}
 	for _, setter := range setters {
 		if err := setter(opt); err != nil {
@@ -39,28 +50,76 @@ func (c *compose) Up(setters ...SetComposeUpOption) error {
 		return NewComposeUpError(err)
 	}
 
-	cmd, err := c.command(flags)
+	cmd, err := c.command(ctx, opt.Writer, flags)
 	if err != nil {
 		return NewComposeUpError(err)
 	}
-
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err = cmd.Run()
-	if err != nil {
-		return NewComposeUpError(err)
-	}
-
-	return nil
+	return handleContextCancellation(ctx, cmd.Run())
 }
 
-func (c *compose) command(args []string) (*exec.Cmd, error) {
+func (c *compose) Down(ctx context.Context, setters ...SetComposeDownOption) error {
+	opt := &ComposeDownOptions{
+		Flags:  []string{"down"},
+		Writer: os.Stdout,
+	}
+	for _, setter := range setters {
+		if err := setter(opt); err != nil {
+			return NewComposeDownError(err)
+		}
+	}
+	flags, err := opt.GenerateFlags()
+	if err != nil {
+		return NewComposeDownError(err)
+	}
+
+	cmd, err := c.command(ctx, opt.Writer, flags)
+	if err != nil {
+		return NewComposeDownError(err)
+	}
+	return handleContextCancellation(ctx, cmd.Run())
+}
+
+// Logs is a function that runs the docker compose logs command
+// it returns an error if the command fails, or nil if the command succeeds
+// it also returns nil if the context is canceled
+func (c *compose) Logs(ctx context.Context, setters ...SetComposeLogsOption) error {
+	opt := &ComposeLogsOptions{
+		Flags:  []string{"logs"},
+		Writer: os.Stdout,
+	}
+	for _, setter := range setters {
+		if err := setter(opt); err != nil {
+			return NewComposeLogsError(err)
+		}
+	}
+	flags, err := opt.GenerateFlags()
+	if err != nil {
+		return NewComposeLogsError(err)
+	}
+
+	cmd, err := c.command(ctx, opt.Writer, flags)
+	if err != nil {
+		return NewComposeLogsError(err)
+	}
+
+	return handleContextCancellation(ctx, cmd.Run())
+}
+func (c *compose) command(ctx context.Context, writer io.Writer, args []string) (*exec.Cmd, error) {
 	file, err := c.project.Marshal()
 	if err != nil {
 		return nil, NewComposeError(err)
 	}
-	cmd := exec.Command("docker", "compose", "-f", "-")
+	cmd := exec.CommandContext(ctx, "docker", "compose", "-f", "-")
 	cmd.Args = append(cmd.Args, args...)
 	cmd.Stdin = strings.NewReader(string(file))
+	cmd.Stdout = writer
+	cmd.Stderr = writer
 	return cmd, nil
+}
+
+func handleContextCancellation(ctx context.Context, err error) error {
+	if errors.Is(err, context.Canceled) || ctx.Err() == context.Canceled {
+		return nil
+	}
+	return err
 }
