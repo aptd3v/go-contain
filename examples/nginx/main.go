2a3009f20709216045d0416f2cfc7c951ecaaa47
aptd3v <aptd3v@proton.me>
2025-07-20 21:41:19 -0400

    feat: add nginx example with Docker image build and service orchestration

---
+++ examples/nginx/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/aptd3v/go-contain/pkg/client"
+	"github.com/aptd3v/go-contain/pkg/client/options/image/build"
+	"github.com/aptd3v/go-contain/pkg/compose"
+	"github.com/aptd3v/go-contain/pkg/compose/options/up"
+	"github.com/aptd3v/go-contain/pkg/create"
+	"github.com/aptd3v/go-contain/pkg/create/config/cc"
+	"github.com/aptd3v/go-contain/pkg/create/config/hc"
+)
+
+func main() {
+	df, err := WithDockerContext("./examples/nginx")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cli, err := client.NewClient(client.FromEnv())
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := cli.ImageBuild(context.Background(), df, build.WithTags("nginx-example:latest"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = io.Copy(os.Stdout, resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	project := create.NewProject("nginx-example")
+	project.WithService("nginx", create.NewContainer().
+		With(
+			cc.WithImage("nginx-example:latest"),
+			cc.WithCommand("nginx", "-g", "daemon off;"),
+			hc.WithPortBindings("tcp", "0.0.0.0", "8080", "80"),
+		),
+	)
+	app := compose.NewCompose(project)
+	err = app.Up(context.Background(), up.WithDetach(), up.WithRemoveOrphans())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("app started at http://localhost:8080")
+}
+func WithDockerContext(path string) (io.Reader, error) {
+	df := create.NewDockerFile()
+	df.From("nginx", "latest")
+	df.Copy("nginx.conf", "/etc/nginx/nginx.conf")
+	df.Copy("index.html", "/usr/share/nginx/html/index.html")
+
+	return df.NewLocalBuildContext(path)
+}
