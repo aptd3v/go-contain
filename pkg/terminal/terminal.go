0ea2f8c92df0d986609b85070bae68eee83830f3
aptd3v <aptd3v@proton.me>
2025-07-21 16:30:42 -0400

    refactor: remove unused terminal size monitoring functions

--- pkg/terminal/terminal.go
+++ pkg/terminal/terminal.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"golang.org/x/term"
 )
@@ -76,29 +74,4 @@ func (s *Session) Close() error {
 		}
 	}
 	return nil
-}
-
-// GetSize returns the current terminal size
-func (s *Session) GetSize() (width, height int, err error) {
-	return term.GetSize(int(s.stdin.Fd()))
-}
-
-// MonitorSize sends new terminal sizes over the channel when SIGWINCH is received.
-func (s *Session) MonitorSize() chan MonitorSize {
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGWINCH)
-
-	go func() {
-		defer close(s.resizeCh)
-
-		for range signals {
-			width, height, err := s.GetSize()
-			if err != nil {
-				return
-			}
-			s.resizeCh <- MonitorSize{Width: uint(width), Height: uint(height)}
-		}
-	}()
-
-	return s.resizeCh
 }
