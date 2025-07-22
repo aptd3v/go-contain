8d3b7e619a1ccc8a2a2cd0ba6923ed505c14ffa9
aptd3v <aptd3v@proton.me>
2025-07-21 21:46:00 -0400

    docs: add documentation comments for error handling functions in the compose package

--- pkg/compose/errors.go
+++ pkg/compose/errors.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 )
 
+// Errors for the compose command and its setters.
 var (
 	ErrComposeError       = fmt.Errorf("compose error")
 	ErrComposeFlagError   = fmt.Errorf("compose flag error")
@@ -29,18 +30,20 @@ func (e *ComposeFlagError) Error() string {
 	return fmt.Sprintf("compose flag error: %s: %s", e.Flag, e.Message)
 }
 
+// NewComposeFlagError makes a new ComposeFlagError with the given flag and message.
 func NewComposeFlagError(flag, message string) *ComposeFlagError {
 	return &ComposeFlagError{
 		Flag:    flag,
 		Message: message,
 	}
 }
+
+// IsComposeFlagError checks if the error is a ComposeFlagError.
 func IsComposeFlagError(err error) bool {
 	return errors.Is(err, ErrComposeFlagError)
 }
 
 // ComposeKillError is the error for the compose kill command
-
 type ComposeKillError struct {
 	Message string
 }
@@ -53,18 +56,19 @@ func (e *ComposeKillError) Error() string {
 	return fmt.Sprintf("compose kill error: %s", e.Message)
 }
 
+// NewComposeKillError creates a new ComposeKillError with the given error message.
 func NewComposeKillError(err error) *ComposeKillError {
 	return &ComposeKillError{
 		Message: err.Error(),
 	}
 }
 
+// IsComposeKillError checks if the error is a ComposeKillError.
 func IsComposeKillError(err error) bool {
 	return errors.Is(err, ErrComposeKillError)
 }
 
 // ComposeEventsError is the error for the compose events command
-
 type ComposeEventsError struct {
 	Message string
 }
@@ -77,12 +81,14 @@ func (e *ComposeEventsError) Error() string {
 	return fmt.Sprintf("compose events error: %s", e.Message)
 }
 
+// NewComposeEventsError creates a new ComposeEventsError with the given error message.
 func NewComposeEventsError(err error) *ComposeEventsError {
 	return &ComposeEventsError{
 		Message: err.Error(),
 	}
 }
 
+// IsComposeEventsError checks if the error is a ComposeEventsError.
 func IsComposeEventsError(err error) bool {
 	return errors.Is(err, ErrComposeEventsError)
 }
@@ -105,6 +111,7 @@ func NewComposeError(err error) *ComposeError {
 	}
 }
 
+// IsComposeError checks if the error is a ComposeError.
 func IsComposeError(err error) bool {
 	return errors.Is(err, ErrComposeError)
 }
@@ -122,12 +129,14 @@ func (e *ComposeUpError) Error() string {
 	return fmt.Sprintf("compose up error: %s", e.Message)
 }
 
+// NewComposeUpError creates a new ComposeUpError with the given error message.
 func NewComposeUpError(err error) *ComposeUpError {
 	return &ComposeUpError{
 		Message: err.Error(),
 	}
 }
 
+// IsComposeUpError checks if the error is a ComposeUpError.
 func IsComposeUpError(err error) bool {
 	return errors.Is(err, ErrComposeUpError)
 }
@@ -145,12 +154,14 @@ func (e *ComposeDownError) Error() string {
 	return fmt.Sprintf("compose down error: %s", e.Message)
 }
 
+// NewComposeDownError creates a new ComposeDownError with the given error message.
 func NewComposeDownError(err error) *ComposeDownError {
 	return &ComposeDownError{
 		Message: err.Error(),
 	}
 }
 
+// IsComposeDownError checks if the error is a ComposeDownError.
 func IsComposeDownError(err error) bool {
 	return errors.Is(err, ErrComposeDownError)
 }
@@ -168,12 +179,14 @@ func (e *ComposeLogsError) Error() string {
 	return fmt.Sprintf("compose logs error: %s", e.Message)
 }
 
+// NewComposeLogsError creates a new ComposeLogsError with the given error message.
 func NewComposeLogsError(err error) *ComposeLogsError {
 	return &ComposeLogsError{
 		Message: err.Error(),
 	}
 }
 
+// IsComposeLogsError checks if the error is a ComposeLogsError.
 func IsComposeLogsError(err error) bool {
 	return errors.Is(err, ErrComposeLogsError)
 }
