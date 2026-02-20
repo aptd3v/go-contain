85d130b9e54fb786ad54a1b201e322c1f66ef9c0
aptd3v <aptd3v@proton.me>
2026-02-19 19:16:34 -0500

    feat: extend error handling in Compose package by adding specific error types for Ps, Start, Stop, Restart, Build, Pull, and Exec commands

--- pkg/compose/errors.go
+++ pkg/compose/errors.go
@@ -12,8 +12,15 @@ var (
 	ErrComposeUpError     = fmt.Errorf("compose up error")
 	ErrComposeDownError   = fmt.Errorf("compose down error")
 	ErrComposeLogsError   = fmt.Errorf("compose logs error")
-	ErrComposeKillError   = fmt.Errorf("compose kill error")
-	ErrComposeEventsError = fmt.Errorf("compose events error")
+	ErrComposeKillError    = fmt.Errorf("compose kill error")
+	ErrComposeEventsError  = fmt.Errorf("compose events error")
+	ErrComposePsError      = fmt.Errorf("compose ps error")
+	ErrComposeStartError   = fmt.Errorf("compose start error")
+	ErrComposeStopError    = fmt.Errorf("compose stop error")
+	ErrComposeRestartError = fmt.Errorf("compose restart error")
+	ErrComposeBuildError  = fmt.Errorf("compose build error")
+	ErrComposePullError   = fmt.Errorf("compose pull error")
+	ErrComposeExecError   = fmt.Errorf("compose exec error")
 )
 
 // ComposeFlagError is the error for the compose flag
@@ -190,3 +197,94 @@ func NewComposeLogsError(err error) *ComposeLogsError {
 func IsComposeLogsError(err error) bool {
 	return errors.Is(err, ErrComposeLogsError)
 }
+
+// ComposePsError is the error for the compose ps command
+type ComposePsError struct {
+	Message string
+}
+
+func (e *ComposePsError) Unwrap() error { return ErrComposePsError }
+func (e *ComposePsError) Error() string { return fmt.Sprintf("compose ps error: %s", e.Message) }
+
+func NewComposePsError(err error) *ComposePsError {
+	return &ComposePsError{Message: err.Error()}
+}
+func IsComposePsError(err error) bool { return errors.Is(err, ErrComposePsError) }
+
+// ComposeStartError is the error for the compose start command
+type ComposeStartError struct {
+	Message string
+}
+
+func (e *ComposeStartError) Unwrap() error { return ErrComposeStartError }
+func (e *ComposeStartError) Error() string { return fmt.Sprintf("compose start error: %s", e.Message) }
+
+func NewComposeStartError(err error) *ComposeStartError {
+	return &ComposeStartError{Message: err.Error()}
+}
+func IsComposeStartError(err error) bool { return errors.Is(err, ErrComposeStartError) }
+
+// ComposeStopError is the error for the compose stop command
+type ComposeStopError struct {
+	Message string
+}
+
+func (e *ComposeStopError) Unwrap() error { return ErrComposeStopError }
+func (e *ComposeStopError) Error() string { return fmt.Sprintf("compose stop error: %s", e.Message) }
+
+func NewComposeStopError(err error) *ComposeStopError {
+	return &ComposeStopError{Message: err.Error()}
+}
+func IsComposeStopError(err error) bool { return errors.Is(err, ErrComposeStopError) }
+
+// ComposeRestartError is the error for the compose restart command
+type ComposeRestartError struct {
+	Message string
+}
+
+func (e *ComposeRestartError) Unwrap() error { return ErrComposeRestartError }
+func (e *ComposeRestartError) Error() string { return fmt.Sprintf("compose restart error: %s", e.Message) }
+
+func NewComposeRestartError(err error) *ComposeRestartError {
+	return &ComposeRestartError{Message: err.Error()}
+}
+func IsComposeRestartError(err error) bool { return errors.Is(err, ErrComposeRestartError) }
+
+// ComposeBuildError is the error for the compose build command
+type ComposeBuildError struct {
+	Message string
+}
+
+func (e *ComposeBuildError) Unwrap() error { return ErrComposeBuildError }
+func (e *ComposeBuildError) Error() string { return fmt.Sprintf("compose build error: %s", e.Message) }
+
+func NewComposeBuildError(err error) *ComposeBuildError {
+	return &ComposeBuildError{Message: err.Error()}
+}
+func IsComposeBuildError(err error) bool { return errors.Is(err, ErrComposeBuildError) }
+
+// ComposePullError is the error for the compose pull command
+type ComposePullError struct {
+	Message string
+}
+
+func (e *ComposePullError) Unwrap() error { return ErrComposePullError }
+func (e *ComposePullError) Error() string { return fmt.Sprintf("compose pull error: %s", e.Message) }
+
+func NewComposePullError(err error) *ComposePullError {
+	return &ComposePullError{Message: err.Error()}
+}
+func IsComposePullError(err error) bool { return errors.Is(err, ErrComposePullError) }
+
+// ComposeExecError is the error for the compose exec command
+type ComposeExecError struct {
+	Message string
+}
+
+func (e *ComposeExecError) Unwrap() error { return ErrComposeExecError }
+func (e *ComposeExecError) Error() string { return fmt.Sprintf("compose exec error: %s", e.Message) }
+
+func NewComposeExecError(err error) *ComposeExecError {
+	return &ComposeExecError{Message: err.Error()}
+}
+func IsComposeExecError(err error) bool { return errors.Is(err, ErrComposeExecError) }
